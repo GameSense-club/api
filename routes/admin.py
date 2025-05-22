c6fbd4cd95475857e96424f02ae691b96f6b2da0
falbue <cyansair05@gmail.com>
2025-05-22 15:50:14 +0500

    Получениние данных пользователя, перенесено в user

--- routes/admin.py
+++ routes/admin.py
@@ -1,15 +1,1 @@
 from .main_routes import *
-
-@api.route('/profile/<int:user_id>', methods=['GET'])
-@auth_decorator('admin')
-def user_profile(user_id):
-    user = SQL_request("SELECT * FROM users WHERE user_id = ?", params=(user_id,), fetch='one')
-    if not user:
-        return jsonify({"error": "Пользователь не найден"}), 404
-
-    return jsonify({
-        "user_id": user['user_id'],
-        "email": user['email'],
-        "first_name": user['first_name'],
-        "balance": user['balance']
-    }), 200
