96bab16593f0152ad9bb1b024018f9df5b38b979
falbue <cyansair05@gmail.com>
2025-05-23 14:17:43 +0500

    Вывод всех пользователей

--- routes/admin.py
+++ routes/admin.py
@@ -1,1 +1,5 @@
-from .main_routes import *
+from .main_routes import *@api.route('/profile/all', methods=['GET'])
+@auth_decorator('admin')
+def profiles():
+    user = SQL_request("SELECT * FROM users", fetch='all')
+    return jsonify(user), 200
